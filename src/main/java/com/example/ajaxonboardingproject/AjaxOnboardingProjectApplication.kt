75d5916016dcd9b503932cefd3170135e2266aa4
OlehSarapuk <olehsarapuk@gmail.com>
2023-08-24 10:58:36 +0300

    changed main method

--- src/main/java/com/example/ajaxonboardingproject/AjaxOnboardingProjectApplication.kt
+++ src/main/java/com/example/ajaxonboardingproject/AjaxOnboardingProjectApplication.kt
@@ -1,14 +1,11 @@
 package com.example.ajaxonboardingproject
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AjaxOnboardingProjectApplication {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(AjaxOnboardingProjectApplication::class.java, *args)
-        }
-    }
+class AjaxOnboardingProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<AjaxOnboardingProjectApplication>(*args)
 }
