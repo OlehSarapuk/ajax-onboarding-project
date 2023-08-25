fc8ea89d837806dbb3babf702d6ecfbe6af90813
OlehSarapuk <olehsarapuk@gmail.com>
2023-08-25 17:57:02 +0300

    managed codestyle

--- src/main/java/com/example/ajaxonboardingproject/model/Role.kt
+++ src/main/java/com/example/ajaxonboardingproject/model/Role.kt
@@ -3,6 +3,7 @@ package com.example.ajaxonboardingproject.model
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.mongodb.core.mapping.Document
 
+@Document("roles")
 data class Role(
     @JsonProperty("name")
     var name: RoleName,
@@ -11,4 +12,3 @@ data class Role(
         ADMIN, USER
     }
 }
-
