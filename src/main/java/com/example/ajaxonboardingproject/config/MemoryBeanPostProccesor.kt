94b3b1537641f63b5a7542573bfccad16bda6766
OlehSarapuk <olehsarapuk@gmail.com>
2023-09-07 08:35:20 +0300

    refactored MemoryCheckBeanPostProcessor so it measure payload size in bytes

--- src/main/java/com/example/ajaxonboardingproject/config/MemoryBeanPostProccesor.kt
+++ src/main/java/com/example/ajaxonboardingproject/config/MemoryBeanPostProccesor.kt
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
+import java.io.ByteArrayOutputStream
+import java.io.ObjectOutputStream
 
-const val MAX_MEMORY_USAGE: Long = 100000
+const val MAX_BYTES_PAYLOAD_SIZE: Long = 100
 
 @Component
 @Suppress("SpreadOperator")
@@ -28,20 +30,21 @@ class MemoryCheckBeanPostProcessor : BeanPostProcessor {
             val enhancer = Enhancer()
             enhancer.setSuperclass(originalBeanClass)
             enhancer.setCallback(MethodInterceptor { _, method, args, _ ->
-                val before = computeUsedMemory()
-                val result = method.invoke(bean, *args)
-                val after = computeUsedMemory()
-                if ((after - before) > MAX_MEMORY_USAGE) {
-                    throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Memory usage is too high")
+                val byteArrayOutputStream = ByteArrayOutputStream()
+                val objectOutputStream = ObjectOutputStream(byteArrayOutputStream)
+                args.map { objectOutputStream.writeObject(it) }
+                objectOutputStream.flush()
+                val payloadSize = byteArrayOutputStream.size()
+                objectOutputStream.close()
+                byteArrayOutputStream.close()
+                if (payloadSize > MAX_BYTES_PAYLOAD_SIZE) {
+                    throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Payload byte size is too high")
                 }
-                result
+                method.invoke(bean, *args)
             })
             return enhancer.create()
 
         }
         return bean
     }
-
-    private fun computeUsedMemory() =
-        Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
 }
