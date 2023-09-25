ae4c19d64276c4bc746380a0d9b9f3a183bc24e9
OlehSarapuk <olehsarapuk@gmail.com>
2023-09-25 13:30:18 +0300

    rebased main branch

--- src/main/java/com/example/ajaxonboardingproject/authentication/dto/UserRegistrationRequestDto.kt
+++ src/main/java/com/example/ajaxonboardingproject/authentication/dto/UserRegistrationRequestDto.kt
@@ -1,12 +1,12 @@
 package com.example.ajaxonboardingproject.authentication.dto
 
-import com.example.ajaxonboardingproject.infrastructure.lib.ValidEmail
+import com.example.ajaxonboardingproject.authentication.lib.ValidEmail
 import jakarta.validation.constraints.Size
 import java.io.Serializable
 
 @Suppress("SerialVersionUIDInSerializableClass")
 data class UserRegistrationRequestDto(
-    @field:ValidEmail()
+    @field:ValidEmail
     val email: String,
     @field:Size(min = 8, max = 40)
     val password: String,
